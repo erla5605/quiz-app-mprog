7ebc94e2184e438254ee39e08814304cf3678156
erla5605 <43401426+erla5605@users.noreply.github.com>
2019-12-25 19:34:19 +0100

    added text to speech for questions

--- app/src/main/java/mprog/project/quizapp/QuestionFragment.java
+++ app/src/main/java/mprog/project/quizapp/QuestionFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +31,7 @@ public class QuestionFragment extends Fragment {
     public static final String ANSWER_EXTRA = "answer_boolean";
     public static final String QUESTION_EXTRA = "answered_question_id";
     public static final String QUESTION_ID_ARG = "question_id";
+    private static final int TTS_REQUEST_CODE = 200;
 
     private Question question;
     private List<Answer> answers;
@@ -37,9 +39,12 @@ public class QuestionFragment extends Fragment {
 
     private TextView questionTextView;
     private ImageButton playVideoButton;
+    private ImageButton ttsButton;
 
     private RadioGroup answersRadioGroup;
     private Button answerQuestionButton;
+
+    private TextToSpeech tts;
 
     public static QuestionFragment newInstance(Long quizId) {
         Bundle args = new Bundle();
@@ -68,31 +73,19 @@ public class QuestionFragment extends Fragment {
         questionTextView.setText(question.getQuestionText());
 
         playVideoButton = v.findViewById(R.id.play_video_button);
+        setUpPlayVideoButton();
 
-        if (question.getType() != Question.QuestionType.VIDEO) {
-            playVideoButton.setVisibility(View.INVISIBLE);
-        } else {
-            playVideoButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Uri uri = Uri.parse(question.getVideo());
-                    uri = Uri.parse("vnd.youtube:" + uri.getQueryParameter("v"));
-                    startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                }
-            });
-        }
+        ttsButton = v.findViewById(R.id.tts_button);
+        ttsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+                startActivityForResult(intent, TTS_REQUEST_CODE);
+            }
+        });
 
         answersRadioGroup = v.findViewById(R.id.answers_radio_group);
-
-        for (Answer answer : answers) {
-            RadioButton rButton = new RadioButton(v.getContext());
-            int radioButtonId = v.generateViewId();
-            rButton.setId(radioButtonId);
-            radioButtonIds.add(radioButtonId);
-            rButton.setText(answer.getAnswerText());
-            rButton.setLayoutParams(new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            answersRadioGroup.addView(rButton);
-        }
+        createRadioButtonsForAnswers(v);
 
         answerQuestionButton = v.findViewById(R.id.answer_question_button);
         answerQuestionButton.setOnClickListener(new View.OnClickListener() {
@@ -111,6 +104,33 @@ public class QuestionFragment extends Fragment {
         return v;
     }
 
+    private void createRadioButtonsForAnswers(View v) {
+        for (Answer answer : answers) {
+            RadioButton rButton = new RadioButton(v.getContext());
+            int radioButtonId = v.generateViewId();
+            rButton.setId(radioButtonId);
+            radioButtonIds.add(radioButtonId);
+            rButton.setText(answer.getAnswerText());
+            rButton.setLayoutParams(new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            answersRadioGroup.addView(rButton);
+        }
+    }
+
+    private void setUpPlayVideoButton() {
+        if (question.getType() != Question.QuestionType.VIDEO) {
+            playVideoButton.setVisibility(View.INVISIBLE);
+        } else {
+            playVideoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri uri = Uri.parse(question.getVideo());
+                    uri = Uri.parse("vnd.youtube:" + uri.getQueryParameter("v"));
+                    startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                }
+            });
+        }
+    }
+
     private void setQuestionResult(int id) {
         int index = radioButtonIds.indexOf(id);
         boolean answeredCorrectly = answers.get(index).isCorrectAnswer();
@@ -119,5 +139,34 @@ public class QuestionFragment extends Fragment {
         extras.putLong(QUESTION_EXTRA, question.getId());
         Intent intent = new Intent().putExtras(extras);
         getActivity().setResult(Activity.RESULT_OK, intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TTS_REQUEST_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                startTTS();
+            } else {
+                installTTSData();
+            }
+        }
+    }
+
+    private void startTTS() {
+        tts = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status == TextToSpeech.SUCCESS){
+                    tts.speak(question.getQuestionText(), TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        });
+    }
+
+    private void installTTSData() {
+        Intent intent = new Intent().setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+        startActivity(intent);
     }
 }
